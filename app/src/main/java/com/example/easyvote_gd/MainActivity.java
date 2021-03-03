42ede1bf765917bc07e54aca0192b72f93f8336b
Glenn Dunlop <glenn.dunlop2@mail.dcu.ie>
2021-03-03 20:49:59 +0000

    Voter and Admin user login

--- app/src/main/java/com/example/easyvote_gd/MainActivity.java
+++ app/src/main/java/com/example/easyvote_gd/MainActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,10 +29,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextEmail, editTextPassword;
     private Button signIn;
 
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference reference;
     private FirebaseUser user;
     private String userID;
 
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,14 +101,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
+        //user = FirebaseAuth.getInstance().getCurrentUser();
+        //reference = FirebaseDatabase.getInstance().getReference("Users");
+
+/*        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();*/
+
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task. isSuccessful()) {
+                if(task.isSuccessful()) {
                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                     if(user.isEmailVerified()){
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                        //startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+
+                        user = FirebaseAuth.getInstance().getCurrentUser();
+                        reference = FirebaseDatabase.getInstance().getReference("Users");
+                        userID = user.getUid();
+
+                        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(snapshot.child("admin").exists()) {
+                                    Boolean admin = snapshot.child("admin").getValue().toString().equals("true");
+                                    if(admin) {
+                                        startActivity(new Intent(MainActivity.this, AdminUser.class));
+                                        finish();
+                                    } else {
+                                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                                        finish();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+
+
+                        //startActivity(new Intent(MainActivity.this, ProfileActivity.class));
                         //startActivity(new Intent(MainActivity.this, AdminUser.class));
+
+
+
+
                     } else {
                         user.sendEmailVerification();
                         Toast.makeText(MainActivity.this, "Check your email verification", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/easyvote_gd/UpdateCandidateList.java
+++ app/src/main/java/com/example/easyvote_gd/UpdateCandidateList.java
@@ -1,15 +1,34 @@
 package com.example.easyvote_gd;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class UpdateCandidateList extends AppCompatActivity {
 
@@ -17,9 +36,10 @@ public class UpdateCandidateList extends AppCompatActivity {
     EditText addCandidateParty;
     EditText addCandidateLocation;
     Button addNewCandidate;
+    ImageView addCandidatePic;
+    int TAKE_IMAGE_CODE = 10001;
 
     DatabaseReference reference;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +52,7 @@ public class UpdateCandidateList extends AppCompatActivity {
         addCandidateParty = (EditText) findViewById(R.id.add_Candidate_Party);
         addCandidateLocation = (EditText) findViewById(R.id.add_Candidate_Location);
         addNewCandidate = (Button) findViewById(R.id.add_Candidate_Btn);
+        addCandidatePic = (ImageView) findViewById(R.id.imageCandidate);
 
         reference = FirebaseDatabase.getInstance().getReference().child("Profiles");
 
@@ -55,5 +76,84 @@ public class UpdateCandidateList extends AppCompatActivity {
         reference.push().setValue(newCandidate);
 
         Toast.makeText(UpdateCandidateList.this, "New Candidate has been inserted", Toast.LENGTH_LONG).show();
+    }
+
+    public void handleImageClick(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) !=null) {
+            startActivityForResult(intent, TAKE_IMAGE_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TAKE_IMAGE_CODE) {
+            switch (resultCode) {
+                case RESULT_OK:
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+                    addCandidatePic.setImageBitmap(bitmap);
+                    handleUpload(bitmap);
+            }
+        }
+    }
+
+    private void handleUpload(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("name")
+                .child(uid + ".jpeg");
+
+        reference.putBytes(baos.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
+
+    }
+
+    private void getDownloadUrl (StorageReference reference) {
+        reference.getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        setUserProfileUrI(uri);
+                    }
+                });
+    }
+
+    private void setUserProfileUrI (Uri uri) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+            .setPhotoUri(uri)
+                .build();
+
+        user.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(UpdateCandidateList.this, "Updated Successfully", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(UpdateCandidateList.this, "Profile Image Fail", Toast.LENGTH_LONG).show();
+                    }
+                });
+
     }
 }
